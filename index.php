4b785a6e23937ad061f522f4cb9df24d0d866f1b
Ian Jarrett <ian@dangilovethat.com>
2014-10-19 12:27:34 -0500

    more changes to php that work but no connection due to other issues;

--- index.php
+++ index.php
@@ -1,14 +1,38 @@
 <?php
 
+echo 'hello -01<br>';
 
-require_once 'config.php';
+require_once'config.php';
+
+
+try{
+
+  // database connection
+
+  $conn = new PDO("mysql:host=$sHost;dbname=$sDb",$sUsername,$sPassword);
+
+}
+
+catch(PDOException $pe)
+
+{
+
+  die('Connection error, because: ' .$pe->getMessage());
+
+}
+
+
+
+
+/*
+
 try
 {
-$oConn = new PDO('mysql:host='.$sHost.';dbname='.$sDb, $sUsername, $sPassword);
+$oConn = new PDO("mysql:host=".$sHost.";dbname=".$sDb, $sUsername, $sPassword);
 
-$oConn-­‐>setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$oStmt = $oConn-­‐>prepare('SELECT data FROM `hello_world`');
-$oResult = $oStmt-­‐>fetchAll();
+$oConn‐>setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$oStmt = $oConn‐>prepare('SELECT data FROM `hello_world`');
+$oResult = $oStmt‐>fetchAll();
 foreach ($oResult as $aRow) {
 print_r($aRow['data']);
 }
@@ -16,6 +40,10 @@ print_r($aRow['data']);
 } catch(PDOException $e) {
 echo 'ERROR:'.$e-­‐>getMessage();
 }
+
+*/
+
+echo 'bye -01';
 
 
 ?>
