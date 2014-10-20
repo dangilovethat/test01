1df84cc9486530336b2e215aecdb51ff460a3859
Ian Jarrett <ian@dangilovethat.com>
2014-10-20 10:57:43 -0500

    New query style that worked locally. Now ttrying on the remote site.

--- index.php
+++ index.php
@@ -14,14 +14,28 @@ $oConn = new PDO("mysql:host=".$sHost.";dbname=".$sDb, $sUsername, $sPassword);
 echo 'mid-01 <br>';
 $oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-echo 'mid-after <br>';
 
+
+$sql = "SELECT * FROM hello_world";
+
+$q   = $oConn->query($sql) or die("failed!");
+
+while($r = $q->fetch(PDO::FETCH_ASSOC)){
+
+  echo $r['data'];
+
+}
+
+
+echo '<br>';
+echo 'mid-after <br> ';
+/*
 $oStmt = $oConn->prepare('SELECT data FROM hello_world');
 $oResult = $oStmt->fetchAll();
 foreach ($oResult as $aRow) {
 print_r($aRow['data']);
 }
-
+*/
 
 } catch(PDOException $e) {
 echo 'ERROR:'.$e->getMessage();
