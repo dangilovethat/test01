8ac92363d870fb9e40d2421b794c0eef27c2189e
Ian Jarrett <ian@dangilovethat.com>
2014-10-20 08:28:05 -0500

    works from local now try remote

--- index.php
+++ index.php
@@ -5,43 +5,29 @@ echo 'hello -01<br>';
 require_once'config.php';
 
 
-try{
 
-  // database connection
-
-  $conn = new PDO("mysql:host=$sHost;dbname=$sDb",$sUsername,$sPassword);
-
-}
-
-catch(PDOException $pe)
-
-{
-
-  die('Connection error, because: ' .$pe->getMessage());
-
-}
-
-
-
-
-/*
 
 try
 {
 $oConn = new PDO("mysql:host=".$sHost.";dbname=".$sDb, $sUsername, $sPassword);
 
-$oConn‐>setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$oStmt = $oConn‐>prepare('SELECT data FROM `hello_world`');
-$oResult = $oStmt‐>fetchAll();
+echo 'mid-01 <br>';
+$oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+echo 'mid-after <br>';
+
+$oStmt = $oConn->prepare('SELECT data FROM hello_world');
+$oResult = $oStmt->fetchAll();
 foreach ($oResult as $aRow) {
 print_r($aRow['data']);
 }
 
+
 } catch(PDOException $e) {
-echo 'ERROR:'.$e-­‐>getMessage();
+echo 'ERROR:'.$e->getMessage();
 }
 
-*/
+
 
 echo 'bye -01';
 
